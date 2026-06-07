0702ebdfbee4f00500d68bcf6a8e6465c8a80288
Shaik Haseena <shaik-haseena@hcltech.com>
2026-06-07 16:36:38 +0530

    Initial commit

---
+++ POC-20-main/main.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"Hello Team from POC-20 Python App hi ")
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", 8080), Handler)
+    print("Server running on port 8080...")
+    server.serve_forever()
